e2b0e81cbcb070e1c57d59f07eda1764a9d6ac1d
MjDev <noemail@noemail.com>
2023-07-16 23:59:42 +0200

    renvoie vers page d'erreur

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,12 +3,13 @@ import '../styles/Gallery.scss'
 import housing from '../data/housing.json'
 import { Link } from 'react-router-dom'
 import { LogementContext } from "../utils/context"
+import { useNavigate } from 'react-router-dom';
 
 function Gallery(){
 
     {/*const [cards1, setCards1] = useState()
     useEffect(() => {
-        fetch(`http://localhost:3000//src/data/housing.json`)
+        fetch(`http://localhost:3000/src/data/housing.json`)
              .then((response) => response.json()
              .then(( data ) => setCards1(JSON.parse(data))
              .catch((error) => console.log(error))
@@ -20,12 +21,20 @@ function Gallery(){
     const [cards, setCards] = useState(logements)
     
     const { dataLogement, changeDataLogement } = useContext(LogementContext)
+
+    const tableID = cards.map((house) => house.id)
+    
+    const navigate = useNavigate()
+
     function handleClick (id, title, cover, pictures, description, host, rating, location, equipments, tags) {
+        if(id !== null && tableID.includes(id)) {
         changeDataLogement (id, title, cover, pictures, description, host, rating, location, equipments, tags)
 
-   
-
         localStorage.setItem('logementSelected', JSON.stringify(id, title, cover, pictures, description, host, rating, location, equipments, tags) );
+        } 
+        else {
+        navigate('/*')
+        }
     } 
 
     return (
